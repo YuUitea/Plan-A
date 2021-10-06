9ca79f1ca013cd058f8c21c72c88705fe0a6aa56
yuyat <dora.da.real@gmail.com>
2021-10-06 11:19:12 -0700

    Add error handling for undefined urls

---
+++ src/components/todo/ErrorHandling.jsx
@@ -0,0 +1,9 @@
+function ErrorComponent() {
+  return (
+      <div>
+        An error occurred. Don't know what to do? Contact support.
+      </div>
+  );
+}
+
+export default ErrorComponent;
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,7 +1,8 @@
 import {Component} from "react/cjs/react.production.min";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import LoginComponent from "./Login";
 import WelcomeComponent from "./Welcome";
+import ErrorComponent from "./ErrorHandling";
 
 class TodoApp extends Component {
   render() {
@@ -9,9 +10,13 @@ class TodoApp extends Component {
         <div className="TodoApp">
           <Router>
             <>
-              <Route path="/" exact component={LoginComponent} />
-              <Route path="/login" component={LoginComponent} />
-              <Route path="/welcome" component={WelcomeComponent} />
+              <Switch>
+                <Route path="/" exact component={LoginComponent}/>
+                <Route path="/login" component={LoginComponent}/>
+                <Route path="/welcome/" component={WelcomeComponent}/>
+                {/* Redirect to error component if path is undefined */}
+                <Route component={ErrorComponent}/>
+              </Switch>
             </>
           </Router>
         </div>
