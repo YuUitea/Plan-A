f022df30caba64f001e30b301eea1abb7682dbc7
yuyat <dora.da.real@gmail.com>
2021-10-30 11:56:52 -0700

    Add basic logout component

---
+++ src/components/todo/Logout.jsx
@@ -0,0 +1,16 @@
+import {Component} from "react/cjs/react.production.min";
+
+class LogoutComponent extends Component {
+  render() {
+    return (
+        <>
+          <h1>You have been logged out</h1>
+          <div className="container-fluid">
+            Thank you for using Plan A!
+          </div>
+        </>
+    );
+  }
+}
+
+export default LogoutComponent;
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,6 +4,7 @@ import LoginComponent from "./Login";
 import WelcomeComponent from "./Welcome";
 import ErrorComponent from "./ErrorHandling";
 import ListTodosComponent from "./ListTodos";
+import LogoutComponent from "./Logout";
 
 class TodoApp extends Component {
   render() {
@@ -11,14 +12,17 @@ class TodoApp extends Component {
         <div className="TodoApp">
           <Router>
             <>
+              <HeaderComponent/>
               <Switch>
                 <Route path="/" exact component={LoginComponent}/>
                 <Route path="/login" component={LoginComponent}/>
+                <Route path="/logout" component={LogoutComponent}/>
                 <Route path="/welcome/:userName" component={WelcomeComponent}/>
                 <Route path="/todos" component={ListTodosComponent}/>
                 {/* Redirect to error component if path is undefined */}
                 <Route component={ErrorComponent}/>
               </Switch>
+              <FooterComponent/>
             </>
           </Router>
         </div>
